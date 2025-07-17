d191841842f230281bfe7c6b0de2149f680d6638
Ren Kakuno <183798776+ren-0637@users.noreply.github.com>
2025-07-17 13:24:36 +0900

    ホーム画像を少し変更

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const images = ["images/uminagomi_dish1.jpg", "images/uminagomi_dish2.jpg", "images/uminagomi_dish3.jpg","images/wonder_dish1.jpg","images/wonder_dish2.jpg","images/wonder_dish3.jpg"];
+const images = ["images/uminagomi_dish1.jpg", "images/uminagomi_dish2.jpg", "images/uminagomi_dish3.jpg","images/wonder_dish_1.jpg","images/wonder_dish_2.jpg","images/wonder_dish_3.jpg"];
 let current = 0;
 const visual = document.querySelector('.main-visual img');
 setInterval(() => {
